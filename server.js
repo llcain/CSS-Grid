7b8b2f86879488f0648950e2b0904384a2203ef6
llcain <llcain2012@gmail.com>
2023-02-12 00:00:40 -0600

    added blog post

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 // })
 
 app.get('/', function(req, res)  {
-    res.send("<h1>Hello Jon</h1>");
-})
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.get('/contact', function(req, res) {
     res.send("<p>Contact me at llcain@gmail.com</p>");
