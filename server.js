5f037655ee1ba8535aa9b551e4a6363931ce4dbb
llcain <llcain2012@gmail.com>
2023-02-11 21:15:32 -0600

    updated server file

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ app.get('/about', function(req, res) {
     res.send("<p>I am a self-taught Fullstack Developer. I enjoy contributing to and fixing issues on open-source projects. I like to learn new technologies and dabble in various Frontend development skils. I am looking to work on Reactjs projects. I've worked on react projects before. Check out my portfolio by clicking on the link below. What can I help you with? I am really good at what I do. Business Idea: I can fix it!! What do you need? Converting figma design to responsive mobile...styling website with CSS, Sass, or Bootstrap...implementing an API using Express...connecting to a database like mongodb</p>")
 })
 
+app.get('/hobbies', function(req, res) {
+    res.send("<ul><li>Coding</li></ul><ul><li>Listening to music</li></ul><ul><li>Taking pictures</li></ul>");
+})
+
+app.get('/projects', function(req, res) {
+    res.send("<ul><li>Portfolio</li></ul><ul><li>My Blog</li></ul><ul><li>To-do List</li></ul>");
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
