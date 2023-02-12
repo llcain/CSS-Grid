fed8267549c642624ca1bbd389bf254b4fcbcc1d
llcain <llcain2012@gmail.com>
2023-02-12 00:23:35 -0600

    created public and CSS folder - moved styles.css

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.get('/news', function(req, res) {
     res.send("<h2>News</h2><p>THis is the coming news</p>");
 })
 
+app.use(express.static('public'));
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
